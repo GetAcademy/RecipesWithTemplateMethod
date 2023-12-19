f0698af6ebf0372da246bc066d977b7f0a63e1db
GET Academy <terje@kolderup.net>
2023-12-19 11:16:15 +0100

    x

---
+++ RecipesWithTemplateMethod/Hvetebakst.cs
@@ -0,0 +1,15 @@
+﻿namespace RecipesWithTemplateMethod
+{
+    internal class Hvetebakst 
+    {
+        public void Tilbred()
+        {
+            Blande();
+            Elte();
+            Heve1();
+            GjørKlarPorsjoner();
+            Heve2();
+            Steke();
+        }
+    }
+}
---
+++ RecipesWithTemplateMethod/Oppskrift.cs
@@ -0,0 +1,24 @@
+﻿namespace RecipesWithTemplateMethod
+{
+    internal class Oppskrift
+    {
+        public void Tilbred()
+        {
+            Blande();
+            GjørKlarPorsjoner();
+            BehandleMedTemperatur();
+        }
+
+        protected virtual void Blande()
+        {
+        }
+
+        protected virtual void GjørKlarPorsjoner()
+        {
+        }
+
+        protected virtual void Behandle()
+        {
+        }
+    }
+}
